c228c611f7e35c0eb0f8bfbe53de593ecd3545a1
Anton <tsymbal044@gmail.com>
2020-09-07 14:10:45 +0300

    Updates

---
+++ precache-manifest.94b4d71ece52af3d1f3d655be8fa159c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150d12b6cc985c28983cd187d9a22774",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6794129ba60d87a87ea8",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "1178d3e689ba5c7711ef",
+    "url": "./static/css/main.bada76a3.chunk.css"
+  },
+  {
+    "revision": "6794129ba60d87a87ea8",
+    "url": "./static/js/2.33cf9eb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.33cf9eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1178d3e689ba5c7711ef",
+    "url": "./static/js/main.93448cba.chunk.js"
+  },
+  {
+    "revision": "79d17474afd20bce64b6",
+    "url": "./static/js/runtime-main.12497659.js"
+  }
+]);
